60e10a664529277718955ded8491862a41c52993
ZetaRift <zetarift@gmail.com>
2015-08-24 02:03:53 -0400

    Length of video added
    Youtube data v3 API uses the ISO 8601 time format for length of videos.

--- mod/yt.py
+++ mod/yt.py
@@ -3,19 +3,49 @@ import time
 import decimal
 import json
 import requests
+import re
  
 #To grab video stats off a youtube video using JSON
 
 #This is broken because youtube changed its API, bullshit changes being made
 
-api_key = "" #Requires an API key
+api_key = "AIzaSyBSssBIzqEs41dlq3_LzVAyHHdBYeeMJ30"
+
+def iso8601_time(timestr): #Youtube data v3 API gives out ISO 8601 time format for length of videos
+ iso_8601_re = re.compile('P(?P<year>[0-9]*Y)?(?P<month>[0-9]*M)?(?P<week>[0-9]*W)?(?P<day>[0-9]*D)?(T)?(?P<hour>[0-9]*H)?(?P<minute>[0-9]*M)?(?P<second>[0-9]*S)?')
+ ftime = re.match(iso_8601_re, timestr)
+ year, month, week, day, hour, minute, second = '','','','','','',''
+ return_time = ''
+ if ftime.group('year'):
+  year = ftime.group('year').rstrip('Y')
+  return_time = return_time + year + 'y '
+ if ftime.group('month'):
+  month = ftime.group('month').rstrip('M')
+  return_time = return_time + month + 'M '
+ if ftime.group('week'):
+  week = ftime.group('week').rstrip('W')
+  return_time = return_time + week + 'w '
+ if ftime.group('day'):
+  day = ftime.group('day').rstrip('D')
+  return_time = return_time + day + 'd '
+ if ftime.group('hour'):
+  hour = ftime.group('hour').rstrip('H')
+  return_time = return_time + hour + 'h '
+ if ftime.group('minute'):
+  minute = ftime.group('minute').rstrip('M')
+  return_time = return_time + minute + 'm '
+ if ftime.group('second'):
+  second = ftime.group('second').rstrip('S')
+  return_time = return_time + second + 's '
+
+ return return_time
  
 def fetch_video_info(video_id):
-    r = requests.get('https://www.googleapis.com/youtube/v3/videos?id={id}&key={api_key}&part=snippet,statistics'.format(id=video_id,api_key=api_key))
+    r = requests.get('https://www.googleapis.com/youtube/v3/videos?id={id}&key={api_key}&part=snippet,statistics,contentDetails'.format(id=video_id,api_key=api_key))
     return json.loads(r.text)
  
 _title = lambda vinfo: vinfo['items'][0]['snippet']['title']
-#_length = lambda video_info: int(video_info['entry']['media$group']['yt$duration']['seconds'])
+_length = lambda vinfo: vinfo['items'][0]['contentDetails']['duration']
 _view_count = lambda vinfo: int(vinfo['items'][0]['statistics']['viewCount'])
 _upvote_count = lambda vinfo: int(vinfo['items'][0]['statistics']['likeCount'])
 _downvote_count = lambda vinfo: int(vinfo['items'][0]['statistics']['dislikeCount'])
@@ -23,9 +53,10 @@ _downvote_count = lambda vinfo: int(vinfo['items'][0]['statistics']['dislikeCoun
  
 def stats_string(video_id):   #something useless
     vinfo = fetch_video_info(video_id)
-    return "[YouTube: {title} | Views: {view_count} | Votes: {upvote_count} up - {downvote_count} down]".format(
+    return "[YouTube: {title} ({length}) | Views: {view_count} | Votes: {upvote_count} up - {downvote_count} down]".format(
         title=_title(vinfo),
         view_count=_view_count(vinfo),
         upvote_count=_upvote_count(vinfo),
-        downvote_count=_downvote_count(vinfo)
+        downvote_count=_downvote_count(vinfo),
+        length=iso8601_time(_length(vinfo))
         )
