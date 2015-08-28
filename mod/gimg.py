e3adbd429d74231a2c9c257690f051e69b804591
ZetaRift <zetarift@gmail.com>
2015-08-27 23:17:33 -0400

    Update gimg.py

--- mod/gimg.py
+++ mod/gimg.py
@@ -1,11 +1,10 @@
-import json
 import requests
 def search(terms):
         newterms = terms.replace(" ", "%20")
         baseurl = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q="
         url = baseurl + newterms
         response = requests.get(url)
-        jsondata = json.loads(response.text)
+        jsondata = response.json()
         try:
                 image = jsondata['responseData']['results'][0]['unescapedUrl']
         except:
