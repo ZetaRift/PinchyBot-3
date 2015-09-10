875ca2496800ba259cd5cb066a0b2bd70215d403
ZetaRift <zetarift@gmail.com>
2015-09-10 16:24:15 -0400

    Create seen.py

---
+++ mod/seen.py
@@ -0,0 +1,38 @@
+import dataset
+
+
+
+class Seen:
+
+ def __init__(self):
+  self.db = dataset.connect('sqlite:///SeenData.db')
+  self.dbtable = self.db["SeenTable"]
+
+ def savefile(self): #Use this upon exit
+  print("Committing changes to database...")
+  self.db.commit()
+
+ def loadfile(self): #Might not be needed
+  print("Loading database")
+  self.db = dataset.connect('sqlite:///SeenData.db')
+  self.dbtable = self.db["SeenTable"]
+
+ def search(self, user, room, replace): #Search for user
+  t = self.dbtable.find_one(Username=user)
+  if replace == True:
+   if t:
+    #Update
+    ID = t['id']
+    self.dbtable.update(dict(id=ID, Username=user, Time=curtime(), Room=room), ['id'])
+   else:
+    #Place new row
+    print("Nothing to replace, placing new row")
+    self.dbtable.insert(dict(Username=user, Time=curtime(), Room=room))
+  else:
+   if t:
+    res_tuple = (t['Username'], t['Time'], t['Room'])
+    return res_tuple
+   else:
+    return None
+   #Search and return
+ 
