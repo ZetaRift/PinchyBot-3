c827bd51b8a9ba14df3271b1d746f8c80d5f6d31
ZetaRift <zetarift@gmail.com>
2015-11-25 01:08:25 -0500

    Positional api_key argument

--- mod/yt.py
+++ mod/yt.py
@@ -6,9 +6,6 @@ import re
  
 #To grab video stats off a youtube video using JSON
 
-
-api_key = "" #As of v3, an API key is required.
-
 def iso8601_time(timestr): #Youtube data v3 API gives out ISO 8601 time format for length of videos
  iso_8601_re = re.compile('P(?P<year>[0-9]*Y)?(?P<month>[0-9]*M)?(?P<week>[0-9]*W)?(?P<day>[0-9]*D)?(T)?(?P<hour>[0-9]*H)?(?P<minute>[0-9]*M)?(?P<second>[0-9]*S)?')
  ftime = re.match(iso_8601_re, timestr)
@@ -38,7 +35,7 @@ def iso8601_time(timestr): #Youtube data v3 API gives out ISO 8601 time format f
 
  return return_time
  
-def fetch_video_info(video_id):
+def fetch_video_info(video_id, api_key):
     r = requests.get('https://www.googleapis.com/youtube/v3/videos?id={id}&key={api_key}&part=snippet,statistics,contentDetails'.format(id=video_id,api_key=api_key))
     return r.json()
  
@@ -49,8 +46,8 @@ _upvote_count = lambda vinfo: int(vinfo['items'][0]['statistics']['likeCount'])
 _downvote_count = lambda vinfo: int(vinfo['items'][0]['statistics']['dislikeCount'])
 #_human_readable_duration = lambda seconds: str(timedelta(seconds=seconds))
  
-def stats_string(video_id):   #something useless
-    vinfo = fetch_video_info(video_id)
+def stats_string(video_id, api_key):   #something useless
+    vinfo = fetch_video_info(video_id, api_key)
     return "[YouTube: {title} | Length: {length} | Views: {view_count} | Votes: {upvote_count} up - {downvote_count} down]".format(
         title=_title(vinfo),
         view_count=_view_count(vinfo),
