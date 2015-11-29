a9a335b2a81d3d675f71d43f39ad95f12a2eff21
ZetaRift <zetarift@gmail.com>
2015-11-28 20:15:14 -0500

    Nextep check if status code is non-200

--- mod/ponycountdown.py
+++ mod/ponycountdown.py
@@ -42,28 +42,31 @@ def nextep():
  
  cur_time = time.time()
  r = requests.get("http://{h}:{p}/newest".format(h=host,p=port))
- j = r.json()
- air_date = datetime.datetime.fromtimestamp(int(j['episode']['air_date'])).strftime('%Y, %B, %d %H:%M')
- air_date_u = int(j['episode']['air_date'])
- not_time = str(air_date)
- tahead = int(j['episode']['air_date']) + 172800 #Usually on the following Monday
- if cur_time >= air_date_u:
-  return "Episode <b>{etitle}</b> (S{s}E{e}) has aired on {time} {tz}".format(
-   etitle=j['episode']['name'],
-   s=str(j['episode']['season']),
-   e=str(j['episode']['episode']),
-   time=not_time,
-   tz=gettimezone()
-   )
-   
- elif cur_time > tahead:
-  return "Next episode air time is unknown"
- 
+ if r.status_code != 200:
+  return "Not available"
  else:
-  return "Next episode: <b>{etitle}</b> (S{s}E{e}) airs on {time} {tz}".format(
-   etitle=j['episode']['name'],
-   s=j['episode']['season'],
-   e=j['episode']['episode'],
-   time=not_time,
-   tz=gettimezone()
-   )
+  j = r.json()
+  air_date = datetime.datetime.fromtimestamp(int(j['episode']['air_date'])).strftime('%Y, %B, %d %H:%M')
+  air_date_u = int(j['episode']['air_date'])
+  not_time = str(air_date)
+  tahead = int(j['episode']['air_date']) + 172800 #Usually on the following Monday
+  if cur_time >= air_date_u:
+   return "Episode <b>{etitle}</b> (S{s}E{e}) has aired on {time} {tz}".format(
+    etitle=j['episode']['name'],
+    s=str(j['episode']['season']),
+    e=str(j['episode']['episode']),
+    time=not_time,
+    tz=gettimezone()
+    )
+   
+  elif cur_time > tahead:
+   return "Next episode air time is unknown"
+ 
+  else:
+   return "Next episode: <b>{etitle}</b> (S{s}E{e}) airs on {time} {tz}".format(
+    etitle=j['episode']['name'],
+    s=j['episode']['season'],
+    e=j['episode']['episode'],
+    time=not_time,
+    tz=gettimezone()
+    )
