656b82610ebef4b80e94733b21f81fe0509bd1ac
ZetaRift <zetarift@gmail.com>
2016-05-05 08:32:47 -0400

    Add forecast

--- mod/wz.py
+++ mod/wz.py
@@ -30,7 +30,7 @@ def savedb():
 def getinfo(sparam, api_key):  #Use this - faster
  sparam = sparam.replace(" ", "_")
  sparam = sparam.replace(", ", ",")
- r = requests.get('http://api.wunderground.com/api/'+api_key+'/conditions/q/'+sparam+'.json')
+ r = requests.get('http://api.wunderground.com/api/'+api_key+'/forecast/conditions/q/'+sparam+'.json')
  if r.status_code != 200:
   return None
  else:
@@ -74,6 +74,47 @@ def info_string(sparam, database_fetch, user, api_key):
     hum=_humid(w_info)
     )
 
+    
+def forecast_string(sparam, database_fetch, user, api_key):
+
+ if database_fetch == True:
+  user = dbtable.find_one(Username=user)
+  if user:
+   f_info = getinfo(user['Location'], api_key)
+  else:
+   return "No location saved for user, use $wf [location] to fetch the conditions or use $wz.add [location] to save the location."
+ else:
+  f_info = getinfo(sparam, api_key)
+ if f_info is None:
+  return None
+ else:
+  return "4 day forecast for <u>{l}</u>: On {d1}, {c1} with a high of {ch1} C ({fh1} F) and a low of {cl1} C ({fl1} F) | On {d2}, {c2} with a high of {ch2} C ({fh2} F) and a low of {cl2} C ({fl2} F) | On {d3}, {c3} with a high of {ch3} C ({fh3} F) and a low of {cl3} C ({fl3} F) | On {d4}, {c4} with a high of {ch4} C ({fh4} F) and a low of {cl4} C ({fl4} F)".format(
+    l=f_info['current_observation']['display_location']['full'],
+    d1=f_info['forecast']['simpleforecast']['forecastday'][0]['date']['weekday'],
+    c1=f_info['forecast']['simpleforecast']['forecastday'][0]['conditions'],
+    ch1=f_info['forecast']['simpleforecast']['forecastday'][0]['high']['celsius'],
+    fh1=f_info['forecast']['simpleforecast']['forecastday'][0]['high']['fahrenheit'],
+    cl1=f_info['forecast']['simpleforecast']['forecastday'][0]['low']['celsius'],
+    fl1=f_info['forecast']['simpleforecast']['forecastday'][0]['low']['fahrenheit'],
+    d2=f_info['forecast']['simpleforecast']['forecastday'][1]['date']['weekday'],
+    c2=f_info['forecast']['simpleforecast']['forecastday'][1]['conditions'],
+    ch2=f_info['forecast']['simpleforecast']['forecastday'][1]['high']['celsius'],
+    fh2=f_info['forecast']['simpleforecast']['forecastday'][1]['high']['fahrenheit'],
+    cl2=f_info['forecast']['simpleforecast']['forecastday'][1]['low']['celsius'],
+    fl2=f_info['forecast']['simpleforecast']['forecastday'][1]['low']['fahrenheit'],
+    d3=f_info['forecast']['simpleforecast']['forecastday'][2]['date']['weekday'],
+    c3=f_info['forecast']['simpleforecast']['forecastday'][2]['conditions'],
+    ch3=f_info['forecast']['simpleforecast']['forecastday'][2]['high']['celsius'],
+    fh3=f_info['forecast']['simpleforecast']['forecastday'][2]['high']['fahrenheit'],
+    cl3=f_info['forecast']['simpleforecast']['forecastday'][2]['low']['celsius'],
+    fl3=f_info['forecast']['simpleforecast']['forecastday'][2]['low']['fahrenheit'],
+    d4=f_info['forecast']['simpleforecast']['forecastday'][3]['date']['weekday'],
+    c4=f_info['forecast']['simpleforecast']['forecastday'][3]['conditions'],
+    ch4=f_info['forecast']['simpleforecast']['forecastday'][3]['high']['celsius'],
+    fh4=f_info['forecast']['simpleforecast']['forecastday'][3]['high']['fahrenheit'],
+    cl4=f_info['forecast']['simpleforecast']['forecastday'][3]['low']['celsius'],
+    fl4=f_info['forecast']['simpleforecast']['forecastday'][3]['low']['fahrenheit']
+    )
 
 
 
